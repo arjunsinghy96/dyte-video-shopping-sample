fc5d5320b7a241f17b5401dd8e200b19206096a8
Arjun Singh Yadav <arjun@opencraft.com>
2023-03-16 17:43:59 +0530

    add build script

--- shopping-frontend/src/api/backend.ts
+++ shopping-frontend/src/api/backend.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { IProduct } from '../types/product';
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL || 'http://localhost:8000';
 
 interface CreateLiveVideoRequestData {
   user_name: string;
